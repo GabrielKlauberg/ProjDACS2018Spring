39cb11149d3d7e1af3cbf2c4388528b1bde63e69
GABRIEL KLAUBERG <gabriel.klauberg@univille.br>
2018-06-04 21:22:02 -0300

    fist commit

---
+++ src/main/java/br/univille/projdacs2018spring/ProjDacas2018SpringApplication.java
@@ -0,0 +1,12 @@
+package br.univille.projdacs2018spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDacas2018SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDacas2018SpringApplication.class, args);
+	}
+}
---
+++ src/main/java/br/univille/projdacs2018spring/controller/PacienteController.java
@@ -0,0 +1,28 @@
+package br.univille.projdacs2018spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.univille.projdacs2018spring.model.Paciente;
+import br.univille.projdacs2018spring.repository.PacienteRepository;
+
+
+@Controller
+@RequestMapping("/paciente")
+public class PacienteController {
+    
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @GetMapping("")
+    public ModelAndView index() {
+        List<Paciente> listaPaciente = this.pacienteRepository.findAll();
+        
+        return new ModelAndView("paciente/index","listapac",listaPaciente);
+    }
+}
